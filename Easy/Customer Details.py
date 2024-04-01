303e669241816c6b765c3d1620da3e5ea4ec8d59
AKil kumar <57595385+akikl@users.noreply.github.com>
2024-04-01 19:07:34 +0530

    Update Customer Details.py

--- Easy/Customer Details.py
+++ Easy/Customer Details.py
@@ -5,4 +5,8 @@ Find the details of each customer regardless of whether the customer made an ord
 
 SELECT c.first_name,c.last_name, c.city,o.order_details  FROM customers c LEFT JOIN orders o  ON c.id=o.cust_id ORDER BY c.first_name, o.order_details;
 
-# Import your libraries import pandas as pd  # Start writing code df=pd.merge(orders,customers,left_on='cust_id',right_on='id',how='right') df[['first_name','last_name','city','order_details']].sort_values(     ['first_name','order_details'])
+# Import your libraries
+import pandas as pd  
+# Start writing code 
+df=pd.merge(orders,customers,left_on='cust_id',right_on='id',how='right') 
+df[['first_name','last_name','city','order_details']].sort_values(['first_name','order_details'])
